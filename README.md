0b80025f78d5e83ac33169566709118a92b4fb1b
Fabio Mantelli <fmmantelli@gmail.com>
2023-05-19 18:13:29 -0300

    feat: first version

--- README.md
+++ README.md
@@ -14,6 +14,11 @@ pnpm dev
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
+Start Docker Container:
+
+docker build -t medfasee-site .
+docker run -it -p 3000:3000 --name medfasee-site medfasee-site
+
 You can start editing the page by modifying `pages/index.tsx`. The page auto-updates as you edit the file.
 
 [API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.ts`.
--- next.config.js
+++ next.config.js
@@ -1,19 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
-
-
-
-// module.exports = {
-//   async rewrites() {
-//     return [
-//       {
-//         source: '/api/:path*',
-//         destination: 'http://150.162.19.214:6152/:path*', // The :path parameter is used here so will not be automatically passed in the query
-//       },
-//     ]
-//   },
+// const nextConfig = {
+//   reactStrictMode: true,
 // }
+
+// module.exports = nextConfig
+
+
+
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://150.162.19.214:6152/:path*', // The :path parameter is used here so will not be automatically passed in the query
+      },
+    ]
+  },
+}
